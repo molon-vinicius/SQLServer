eff363b1356939a053521419241b1710c98708aa
Vinicius Molon <molon.vinicius@gmail.com>
2025-04-11 15:51:03 -0300

    create_table_bkp_jobs.sql

---
+++ Bkps/Jobs/create_table_bkp_jobs.sql
@@ -0,0 +1,48 @@
+create table [master].[dbo].[tb_bkp_jobs] 
+            ( [ID] numeric(15) identity
+            , [JobID] uniqueidentifier
+            , [JobName] nvarchar(200)
+            , [JobEnabled] char
+            , [JobDescription] nvarchar(1000)
+            , [InsertDate] datetime
+            , [UpdateDate] datetime
+            , [StartStepID] int
+            , [CategoryName] nvarchar(100)
+            , [OwnerName] nvarchar(100)
+            , [OperatorName] nvarchar(100)
+            , [EmailAddress] nvarchar(500)
+            , [SQLCommand] nvarchar(max))
+
+create table [master].[dbo].[tb_bkp_jobs_steps] 
+            ( [JobID] uniqueidentifier
+            , [JobName] nvarchar(200)
+            , [StepID] int
+            , [StepName] nvarchar(max)
+            , [InsertDate] datetime
+            , [UpdateDate] datetime
+            , [ServerName] nvarchar(max)
+            , [DatabaseName] nvarchar(max)
+            , [StepUID] uniqueidentifier 
+            , [SQLCommand] nvarchar(max))
+
+create table [master].[dbo].[tb_bkp_jobs_schedules] 
+            ( [JobID] uniqueidentifier
+            , [JobName] nvarchar(200)
+            , [ScheduleID] int
+            , [ScheduleUID] uniqueidentifier
+            , [ScheduleName] varchar(200) 
+            , [ScheduleEnabled] char
+            , [InsertDate] datetime
+            , [UpdateDate] datetime
+            , [SQLCommand] nvarchar(max))
+
+create table [master].[dbo].[tb_bkp_jobs_alerts] 
+           ( [AlertID] int	
+           , [AlertName] nvarchar(max)	
+           , [AlertEnabled] tinyint	
+           , [InsertDate] datetime
+           , [UpdateDate] datetime
+           , [DatabaseName] nvarchar(512)	
+           , [JobID] uniqueidentifier
+           , [JobName] varchar(200)
+           , [SQLCommand] nvarchar(max))
